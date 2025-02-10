31b3f3300fff459dcaeb3f663cd1a22079941a0c
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2025-02-10 12:40:09 +0100

    fix: payload type import

--- wrapper.ts
+++ wrapper.ts
@@ -13,11 +13,11 @@ import EventWrapper from '@/channel/lib/EventWrapper';
 import { FileType } from '@/chat/schemas/types/attachment';
 import {
   IncomingMessageType,
-  PayloadType,
   StdEventType,
   StdIncomingMessage,
 } from '@/chat/schemas/types/message';
 import { Payload } from '@/chat/schemas/types/quick-reply';
+import { PayloadType } from '@/chat/schemas/types/button';
 
 import { SlackHandler } from './index.channel';
 import { SLACK_CHANNEL_NAME } from './settings';
